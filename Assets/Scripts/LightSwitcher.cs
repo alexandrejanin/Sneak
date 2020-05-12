0e2c4c140f4ab795d071d045396dfbdd5c217c57
alexandrejanin <alexandrejanin92@gmail.com>
2020-05-12 22:38:41 +0200

    display active lights count

--- Assets/Scripts/LightSwitcher.cs
+++ Assets/Scripts/LightSwitcher.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class LightSwitcher : MonoBehaviour {
     [SerializeField] private float updateRate = 10f;
     [SerializeField] private float activationRadius = 30f;
+    [SerializeField] private Text text;
 
     private float timer;
+
+    private void Awake() {
+        text = GameObject.Find("ActiveLightsCount").GetComponent<Text>();
+    }
 
     private void Update() {
         timer += Time.deltaTime;
@@ -16,9 +22,19 @@ public class LightSwitcher : MonoBehaviour {
     }
 
     private void UpdateLights() {
+        var activeCount = 0;
         foreach (var light in FindObjectsOfType<Light>()) {
             light.enabled = (light.transform.position - transform.position).sqrMagnitude
                             < activationRadius * activationRadius;
+            if (light.enabled)
+                activeCount++;
         }
+
+        if (text)
+            text.text = $"{activeCount} active lights";
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.DrawWireSphere(transform.position, activationRadius);
     }
 }
